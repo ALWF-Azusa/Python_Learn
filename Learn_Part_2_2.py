d9939b8d1d00a1718ab2099cb0719736181b289a
shih-hsuan <kevin0921799052@gmail.com>
2024-02-15 02:35:55 +0800

    Note_pyLearn

---
+++ Learn_Part_2_2.py
@@ -0,0 +1,26 @@
+'''
+隨堂練習
+'''
+# EP1
+'''
+A = 1
+A = "happy"
+print(type(A))
+'''
+# EP2
+"""
+A, B, C = 10, 20, 40
+print((A + B + C) / 3)
+"""
+# EP3
+"""
+X = Y = Z = 100
+X = X + 5
+print(X, Y, Z)
+"""
+# EP4
+"""
+Q = Q + 2
+
+"""
+# Error：name 'Q' is not defined.
